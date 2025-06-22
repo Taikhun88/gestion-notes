393d1aa7adf9aba6f7fbfe777331aed85e88df07
Tai KHUN <taiyang78190@gmail.com>
2025-06-22 20:42:29 +0200

    drop down menu with dynamic icons

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useAppSelector } from '@/store/hooks';
 import React from 'react'
+import NoteCategoryDropdown from '@/components/NoteCategoryDropdown';
 
 export default function NoteCard() {
   const notes = useAppSelector((state) => state.notes.notes);
@@ -15,7 +16,10 @@ export default function NoteCard() {
     <div>
       {notes.map((note) => (
         <div className={"NoteCard " + categoryColors[note.category.color]} key={note.id}>
-          <h2 className="CardTitle">{note.title}</h2>
+          <div className="flex items-center items-baseline justify-between mb-3">
+            <h2 className="CardTitle">{note.title}</h2>
+            <NoteCategoryDropdown idCategory={note.category.id}/>
+          </div>
           <p className="CardContent">{note.content}</p>
         </div>
       ))}
